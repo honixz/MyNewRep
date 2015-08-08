0a713f610083e126632a8f3c386b1ce1ffa9df62
honixz <jekacpp@gmail.com>
2015-08-08 12:54:29 +0300

    New changes

--- src/A.java
+++ src/A.java
@@ -5,5 +5,10 @@ public class A {
 
     public static void main(String[] args) {
         System.out.println("Hello World in Git");
+
+        int[]arr = {1,4,5,6,1,2,3};
+        for (int i : arr){
+            System.out.println(i);
+        }
     }
 }
